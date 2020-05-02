2962f3ba6cb69c594c84fdf62949f88f8021cc2e
John (Juan) Lema <jlema@users.noreply.github.com>
2020-05-02 01:42:19 -0700

    Added loading activity indicator

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
-console.disableYellowBox = true;
+
 import React from 'react';
 import Main from './components/MainComponent';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
+import { YellowBox } from 'react-native';
+
+YellowBox.ignoreWarnings(['Warning: ...']);
+console.disableYellowBox = true;
 
 const store = ConfigureStore();
 
